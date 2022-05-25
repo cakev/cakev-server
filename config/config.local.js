8077edf6d39818be8ebaacf017c6576956d3d4ae
zhepei.shen <zhepei.shen@eslink.com>
2022-05-25 14:05:01 +0800

    feat: 🎸 mongodbUrl

--- config/config.local.js
+++ config/config.local.js
@@ -2,7 +2,7 @@ module.exports = () => {
 	const config = {}
 	const mongodbUrl = JSON.parse(process.argv[2]).mongodbUrl
 	config.mongoose = {
-		url: mongodbUrl ? mongodbUrl : `mongodb://cakev:cakev@127.0.0.1/cakev`,
+		url: mongodbUrl ? mongodbUrl : `mongodb://cakev:cakev@127.0.0.1:12770/cakev`,
 		options: {
 			useUnifiedTopology: true,
 		},
